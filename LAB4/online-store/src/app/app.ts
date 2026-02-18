fe56385af6efdd845e9b3b965e35be1049577ee4
Scaenly <eraqwe150@gmail.com>
2026-02-18 13:37:09 +0500

    Lab 4

---
+++ LAB4/online-store/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProductList } from './components/product-card/product-list/product.list';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ProductList],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class App { }
